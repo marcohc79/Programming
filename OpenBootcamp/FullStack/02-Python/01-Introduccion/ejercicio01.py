b5a5948e07e7bd6a583e2d0d309ad180df090c94
Marco Huamani <mhcaballero79@gmail.com>
2022-06-30 18:11:44 -0300

    Comienzo de Ptyhon en el bootcamp

---
+++ OpenBootcamp/FullStack/02-Python/01-Introduccion/ejercicio01.py
@@ -0,0 +1,9 @@
+# Desde la consola de python, debes de imprimir las siguientes cadenas de texto:
+# Hola, soy [tu_nombre]
+# Estoy empezando el curso de python
+# Espero aprender mucho
+
+def holaMundo(name):
+    print(f'Hola, soy ${name}. \nEstoy empezando el curso de Python \nEspero aprender mucho.')
+
+holaMundo('Marco')
